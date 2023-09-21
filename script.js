2baeb147c51225ac59f4162ad253b22c31dc12b9
godswillnwabu <godswillnwabeke50@gmail.com>
2023-09-21 17:00:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// CODE FOR THE HAMBURGER MENU
+let hambugerIcon = document.querySelector(".hamburger-icon");
+let navMenu = document.querySelector(".nav-menu");
+
+hambugerIcon.addEventListener("click", responsiveView)
+
+function responsiveView() {
+    hambugerIcon.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+// CODE FOR THE NAME SLIDES
+document.querySelectorAll(".slide").forEach(function(node) {
+    // Do something with node
+    node.onanimationend = () => {
+        node.style.opacity = 1;
+    }
+  });
+
+const fadeIn = document.querySelector(".heading");
+fadeIn.onanimationend = () => {
+    fadeIn.style.opacity = 1;
+};
+
+// document.querySelector("nav-item").addEventListener('click', function() {
+//     window.scrollTo({
+//       top: 0,
+//       left: 0,
+//       behavior: 'smooth'
+//     });
+// });
+
