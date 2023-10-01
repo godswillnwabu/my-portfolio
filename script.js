1012dd5d4569a5734db17ae1c7132569c1031b04
godswillnwabu <godswillnwabeke50@gmail.com>
2023-10-01 14:31:29 +0100

    Added links to my resume, projects and forms too

--- script.js
+++ script.js
@@ -22,10 +22,3 @@ fadeIn.onanimationend = () => {
     fadeIn.style.opacity = 1;
 };
 
-//     window.scrollTo({
-//       top: 0,
-//       left: 0,
-//       behavior: 'smooth'
-//     });
-// });
-
