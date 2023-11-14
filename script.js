024e8e05d71930d584269e83abfae12fe72a2caf
godswillnwabu <godswillnwabeke50@gmail.com>
2023-11-14 15:28:25 +0100

    Added a javascript to links on smooth scrolling

--- script.js
+++ script.js
@@ -22,3 +22,35 @@ fadeIn.onanimationend = () => {
     fadeIn.style.opacity = 1;
 };
 
+
+// CODE FOR REVEALING OF THE LINK TO HOME
+const linkToHome = document.querySelector(".link-to-home");
+
+window.addEventListener("scroll", function() {
+    const scrollHeigth = window.scrollY;
+    
+    if (scrollHeigth > 2000) {
+        linkToHome.classList.add("show-link");
+    }
+    else {
+        linkToHome.classList.remove("show-link");
+    }
+});
+
+
+// CODE FOR SMOOTH SCROLLING ON PAGE
+let navLinks = document.querySelectorAll(".nav-link");
+
+for (const link of navLinks) {
+    link.addEventListener("click", smoothScroll);
+}
+
+function smoothScroll(event) {
+    event.preventDefault();
+
+    const href = this.getAttribute("href");
+    
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth"
+    });
+}
